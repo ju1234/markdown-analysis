5c2e1e5684b84d8f4488ab9f8c91af8786d7cbc2
ju1234 <596117054@qq.com>
2017-06-08 23:13:24 +0800

    add reg H1

--- Reg/index.js
+++ Reg/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by jufei on 2017/6/8.
  */
+
+
+/**
+ * 匹配标题1 #
+ * @type {RegExp}
+ */
+var _h1 = /(?:^|\n)#\s(.+)(?:\n|$)/gi;
+
+
+
+
+
+export default {
+  _h1,
+}
--- analysis.js
+++ analysis.js
@@ -2,14 +2,17 @@
  * Created by jufei on 2017/6/8.
  */
 
+// import Reg from './Reg';
+//
+// function markDownAnalysis(value) {
+//   var h1Text = Reg.h1Reg.exec(value);
+//   console.log(h1Text);
+//   // return `a${{h1Text[0]}}`
+//   return `<h1>${h1Text[3]}</h1>`
+// }
 
-var h1Reg = /#\s+(.+)\n|\n#\s+(.+)$|#\s+(.+)/gi;
+var markDownAnalysis = 1;
 
-function markDownAnalysis(value) {
-  var h1Text = h1Reg.exec(value);
-  console.log(h1Text)
-  // return `a${{h1Text[0]}}`
-  return `<h1>${h1Text[3]}</h1>`
-}
+console.log('asdas')
 
 export default markDownAnalysis;
