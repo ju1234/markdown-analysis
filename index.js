d4dd668b78027f6ed86455e61b60b8fc9edfe699
ju1234 <596117054@qq.com>
2017-06-11 01:43:34 +0800

    toHTMl can convert script tags to empty

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import Reg from './Reg';
 
 
 function toHTML(value) {
-  console.log(Reg._h1.exec(value));
   // 验证value是否为String类型，如果不是抛出异常
   if (Object.prototype.toString.call(value) !== '[object String]') {
     throw 'toHTML arguments[0] must be string'
@@ -20,7 +19,8 @@ function toHTML(value) {
     throw 'argument[0] is empty'
   }
 
-  value = value
+  value =  value
+      .replace(Reg._script,'')
       .replace(Reg._h1, '\n<h1>$1</h1>\n')
       .replace(Reg._h2, '\n<h2>$1</h2>\n')
       .replace(Reg._h3, '\n<h3>$1</h3>\n')
@@ -30,6 +30,7 @@ function toHTML(value) {
       .replace(Reg._hr, '\n<hr/>\n')
       .replace(Reg._italic, '<i>$1</i>');
 
+  console.log(value);
 
   return value;
 }
